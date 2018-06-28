4aba58ecabedeeb25dd6001fcf94e5596c60eaf4
rdferrari <rd.ferrari@gmail.com>
2018-06-28 17:47:18 -0300

    Basic react-router

--- client/main.js
+++ client/main.js
@@ -1,10 +1,24 @@
 import { Meteor } from "meteor/meteor";
 import React from "react";
 import { render } from "react-dom";
+import { Router, Route, browserHistory } from "react-router";
 
+import Home from "../imports/ui/Home";
+import Login from "../imports/ui/Login";
 import Signup from "../imports/ui/Signup";
 import Link from "../imports/ui/Link";
+import NotFound from "../imports/ui/NotFound";
+
+const routes = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Home} />
+    <Route path="/login" component={Login} />
+    <Route path="/signup" component={Signup} />
+    <Route path="/links" component={Link} />
+    <Route path="/*" component={NotFound} />
+  </Router>
+);
 
 Meteor.startup(() => {
-  render(<Link />, document.getElementById("app"));
+  render(routes, document.getElementById("app"));
 });
