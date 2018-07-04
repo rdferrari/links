f836a59222788327df8ebae18ee39a0be1819b8d
rdferrari <rd.ferrari@gmail.com>
2018-07-04 14:32:14 -0300

    Component State basic

--- imports/ui/Signup.js
+++ imports/ui/Signup.js
@@ -2,6 +2,22 @@ import React from "react";
 import { Link } from "react-router";
 
 export default class Signup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
+  increment() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+  decrement() {
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
   render() {
     return (
       <div>
@@ -12,6 +28,9 @@ export default class Signup extends React.Component {
         <Link to="/">
           <p>link to Home</p>
         </Link>
+        <p>{this.state.count}</p>
+        <button onClick={this.increment.bind(this)}>+</button>
+        <button onClick={this.decrement.bind(this)}>-</button>
       </div>
     );
   }
