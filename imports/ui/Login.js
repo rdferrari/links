38c799b80e2d44177b047e2f5fa2026a59ca0589
rdferrari <rd.ferrari@gmail.com>
2018-07-04 15:36:51 -0300

    Login

--- imports/ui/Login.js
+++ imports/ui/Login.js
@@ -1,7 +1,24 @@
+import { Meteor } from "meteor/meteor";
 import React from "react";
 import { Link } from "react-router";
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: ""
+    };
+  }
+  onSubmit(e) {
+    e.preventDefault();
+
+    let email = this.refs.email.value.trim();
+    let password = this.refs.password.value.trim();
+
+    Meteor.loginWithPassword({ email }, password, err => {
+      console.log("login callback", err);
+    });
+  }
   render() {
     return (
       <div>
@@ -12,6 +29,17 @@ export default class Login extends React.Component {
         <Link to="/">
           <p>link to Home</p>
         </Link>
+        {this.state.error ? <p>{this.state.error}</p> : undefined}
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="email" ref="email" name=" email" placeholder="Email" />
+          <input
+            type="password"
+            ref="password"
+            name="password"
+            placeholder="Password"
+          />
+          <button>Login</button>
+        </form>
       </div>
     );
   }
