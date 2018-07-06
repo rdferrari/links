4972f018c5f0ef431729adc744f5ee9e5e4edc65
rdferrari <rd.ferrari@gmail.com>
2018-07-06 13:11:09 -0300

    map() LinksList with Tracker.autorun

--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Accounts } from "meteor/accounts-base";
 
 import { Links } from "../api/links";
+import LinksList from "./LinksList";
 
 export default class Link extends React.Component {
   onLogout() {
@@ -26,6 +27,7 @@ export default class Link extends React.Component {
       <div>
         <h1>Your Links</h1>
         <button onClick={this.onLogout.bind(this)}>Logout</button>
+        <LinksList />
         <p>Add Link</p>
         <form onSubmit={this.onSubmit.bind(this)}>
           <input type="text" ref="url" placeholder="URL" />
---
+++ imports/ui/LinksList.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Tracker } from "meteor/tracker";
+
+import { Links } from "../api/links";
+
+export default class LinksList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myLinks: []
+    };
+  }
+  componentDidMount() {
+    console.log("componentDidMount");
+    this.linksTracker = Tracker.autorun(() => {
+      const myLinks = Links.find().fetch();
+      this.setState({ myLinks });
+    });
+  }
+
+  componentWillUnmount() {
+    console.log("Unmount");
+    this.linksTracker.stop();
+  }
+
+  renderLinksListItens() {
+    return this.state.myLinks.map(link => {
+      return <p key={link._id}>{link.url}</p>;
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <p>LinkList</p>
+        <div>{this.renderLinksListItens()}</div>
+      </div>
+    );
+  }
+}
