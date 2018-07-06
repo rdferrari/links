b5ec7f67748cef7f44389121e836d74bb52ef7c0
rdferrari <rd.ferrari@gmail.com>
2018-07-06 12:05:51 -0300

    Adding links to MongoCollection

--- client/main.js
+++ client/main.js
@@ -3,10 +3,16 @@ import { render } from "react-dom";
 import { Tracker } from "meteor/tracker";
 
 import { routes, onAuthChange } from "../imports/routes/routes";
+import { Links } from "../imports/api/links";
 
 Tracker.autorun(() => {
   const isAuthenticated = !!Meteor.userId();
   onAuthChange(isAuthenticated);
+});
+
+Tracker.autorun(() => {
+  const myLinks = Links.find().fetch();
+  console.log(myLinks);
 });
 
 Meteor.startup(() => {
--- imports/ui/Link.js
+++ imports/ui/Link.js
@@ -1,15 +1,36 @@
 import React from "react";
 import { Accounts } from "meteor/accounts-base";
 
+import { Links } from "../api/links";
+
 export default class Link extends React.Component {
   onLogout() {
     Accounts.logout();
   }
+  onSubmit(e) {
+    // this get the valuer from the input
+    const url = this.refs.url.value.trim();
+    // the trim clean the spaces of the input
+
+    e.preventDefault();
+    // if there is a url imput
+    if (url) {
+      // insert the url value
+      Links.insert({ url });
+      // then clear the value
+      this.refs.url.value = "";
+    }
+  }
   render() {
     return (
       <div>
-        <h1>Links</h1>
+        <h1>Your Links</h1>
         <button onClick={this.onLogout.bind(this)}>Logout</button>
+        <p>Add Link</p>
+        <form onSubmit={this.onSubmit.bind(this)}>
+          <input type="text" ref="url" placeholder="URL" />
+          <button>Add Link</button>
+        </form>
       </div>
     );
   }
