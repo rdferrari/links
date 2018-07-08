c87a2cf7b4363a8ca6a1355902d8c41607f80c3c
rdferrari <rd.ferrari@gmail.com>
2018-07-08 09:40:52 -0300

    Redirect middleware

--- server/main.js
+++ server/main.js
@@ -2,22 +2,21 @@ import { Meteor } from "meteor/meteor";
 import { WebApp } from "meteor/webapp";
 
 import "../imports/api/users";
-import "../imports/api/links";
+import { Links } from "../imports/api/links";
 import "../imports/startup/simple-schema-config.js";
 
 Meteor.startup(() => {
   // code to run on server at startup
   WebApp.connectHandlers.use((req, res, next) => {
-    console.log("This is my middleware");
-    console.log(req.url, req.method, req.headers, req.query);
-    // Set HTTP status headers
-    // res.statusCode = 404;
-    // Set HTTP headres
-    // res.setHeader("my-header", "I was here");
-    // Set HTTP body
-    // res.write("<h1>This my middleware</h1>");
-    // End HTTP request
-    // res.end();
-    next();
+    const _id = req.url.slice(1);
+    const link = Links.findOne({ _id });
+
+    if (link) {
+      res.statusCode = 302;
+      res.setHeader("Location", link.url);
+      res.end();
+    } else {
+      next();
+    }
   });
 });
