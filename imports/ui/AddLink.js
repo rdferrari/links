66587a24351168abecd133fa4e6c6736f611dd28
rdferrari <rd.ferrari@gmail.com>
2018-07-09 10:52:24 -0300

    Controlled inputs - essential to update

--- imports/ui/AddLink.js
+++ imports/ui/AddLink.js
@@ -4,27 +4,47 @@ import { Meteor } from "meteor/meteor";
 import { Links } from "../api/links";
 
 export default class AddLink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: "Hey"
+    };
+  }
   onSubmit(e) {
     // this get the valuer from the input
-    const url = this.refs.url.value.trim();
+    // const url = this.refs.url.value.trim();
     // the trim clean the spaces of the input
+    const { url } = this.state;
 
     e.preventDefault();
     // if there is a url imput
     if (url) {
-      Meteor.call("links.insert", url);
+      Meteor.call("links.insert", url, (err, res) => {
+        if (!err) {
+          this.setState({ url: "" });
+        }
+      });
       // insert the url value
       // Links.insert({ url, userId: Meteor.userId() });
       // then clear the value
-      this.refs.url.value = "";
     }
+  }
+  onChange(e) {
+    this.setState({
+      url: e.target.value
+    });
   }
   render() {
     return (
       <div>
         <p>Add Link</p>
         <form onSubmit={this.onSubmit.bind(this)}>
-          <input type="text" ref="url" placeholder="URL" />
+          <input
+            type="text"
+            placeholder="URL"
+            value={this.state.url}
+            onChange={this.onChange.bind(this)}
+          />
           <button>Add Link</button>
         </form>
       </div>
--- imports/ui/LinksListFilter.js
+++ imports/ui/LinksListFilter.js
@@ -1,19 +1,40 @@
 import React from "react";
 import { Session } from "meteor/session";
+import { Tracker } from "meteor/tracker";
 
-export default () => {
-  return (
-    <div>
-      <label>
-        <input
-          type="checkbox"
-          onChange={e => {
-            Session.set("showVisible", !e.target.checked);
-            // console.log(e.target.checked);
-          }}
-        />
-        Show hidden links
-      </label>
-    </div>
-  );
-};
+export default class LinksListFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showVisible: true
+    };
+  }
+  componentDidMount() {
+    this.tracker = Tracker.autorun(() => {
+      this.setState({
+        showVisible: Session.get("showVisible")
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.tracker.stop();
+  }
+  render() {
+    return (
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={!this.state.showVisible}
+            onChange={e => {
+              Session.set("showVisible", !e.target.checked);
+              // console.log(e.target.checked);
+            }}
+          />
+          Show hidden links
+        </label>
+      </div>
+    );
+  }
+}
