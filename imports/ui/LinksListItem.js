a599b67e8f742f136d5ae756e1cd8c421fb9dddd
rdferrari <rd.ferrari@gmail.com>
2018-07-09 00:06:10 -0300

    Clipboard npm library

--- imports/ui/LinksListItem.js
+++ imports/ui/LinksListItem.js
@@ -1,12 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Clipboard from "clipboard";
 
 export default class LinksListItem extends React.Component {
+  componentDidMount() {
+    this.clipboard = new Clipboard(this.refs.copy);
+
+    this.clipboard
+      .on("success", () => {
+        alert("Worked");
+      })
+      .on("error", () => {
+        alert("Ops");
+      });
+  }
+  componentWillUnmount() {
+    this.clipboard.destroy();
+  }
   render() {
     return (
       <div>
         <p>{this.props.url}</p>
         <p>{this.props.shortUrl}</p>
+        <button ref="copy" data-clipboard-text={this.props.shortUrl}>
+          Copy
+        </button>
       </div>
     );
   }
